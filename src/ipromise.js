ffe73c1f6d1d5d4f03733b3d30ce817de07096ac
wuzijie <wuzijie_1991@163.com>
2015-04-30 17:06:37 +0800

    changed

--- src/ipromise.js
+++ src/ipromise.js
@@ -1,165 +1,18 @@
-(function () {
+window.iPromise = (function() {
 
-  var PENDING = 0, FULFILLED = 1, REJECTED = 2;
+  var FULFILLED = 1, REJECTED = 2, PENDING = 0;
 
-  var utils = {
-    isFunction: function(obj) {
-      return Object.prototype.toString.call(obj) === '[object Function]';
-    }
-  };
-
-  var fulfill = function() {
-    this.state = FULFILLED;
-    for (var i = 0, len = this.handlers.length; i < len; ++i) {
-      var fulfilled = this.handlers[i].onFulfilled;
-      if (utils.isFunction(fulfilled)) {
-        fulfilled(this.value);
+  function getThen(value) {
+    var t = typeof value;
+    if (value && (t === 'object' || t === 'function')) {
+      var then = value.then;
+      if (typeof then === 'function') {
+        return then;
       }
     }
-  };
+    return null;
+  }
 
-  var reject = function() {
-    this.state = REJECTED;
-    for (var i = 0, len = this.handlers.length; i < len; ++i) {
-      var rejected = this.handlers[i].onRejected;
-      if (utils.isFunction(rejected)) {
-        rejected(this.value);
-      }
-    }
-  };
-
-  /**
-   * iPromise
-   * @constructor
-   * @param {Function} fn fn带有两个参数，一个是成功回调的执行函数，另一个是失败回调的执行函数
-   */
-  var iPromise = window.iPromise = function(fn) {
-    this.state = PENDING;
-    this.value = null;
-    this.handlers = [];
-    var self = this;
-    doResolve(fn, resolve, reject);
-  };
-
-  /**
-   * 根据传入参数是否为thenable来执行fulfill或者doResolve，如果出错就执行reject
-   * @param {any} result
-   */
-  var resolve = function(result) {
-    try {
-        var then = getThen(result);
-        if (then) {
-          doResolve(then.bind(result), resolve, reject)
-          return
-        }
-        fulfill(result);
-      } catch (e) {
-        reject(e);
-      }
-  };
-
-  iPromise.reject = function() {};
-
-  iPromise.all = function() {};
-
-  iPromise.prototype.done = function(onFulfilled, onRejected) {
-    var self = this;
-    // 保证所有回调都是异步的
-    setTimeout(function() {
-      handle.call(self, {
-        onFulfilled: onFulfilled,
-        onRejected: onRejected
-      });
-    }, 0);
-  };
-
-  /**
-   * then
-   * @param {Function} onFulfilled
-   * @param {Function} onRejected
-   * @return {Object} a new iPromise instance
-   */
-  iPromise.prototype.then = function(onFulfilled, onRejected) {
-    var self = this;
-    return new iPromise(function(resolve, reject) {
-      // then方法总是返回一个新的Promise对象，这个新的Promise对象用于链式传递
-      // 为了保证下一个Promise的回调在当前Promise的回调之后执行（链式执行）
-      self.done(function(result) {
-        // 如果onFulFilled是一个函数（必定有一个返回值x）
-        if (utils.isFunction(onFulfilled)) {
-          try {
-            resolve(onFulfilled(result));
-          } catch (ex) {
-            reject(ex);
-          }
-        } else { // 如果onFulfilled不是一个函数，但上一级Promise成功执行
-          resolve(result);
-        }
-      }, function(error) {
-        // 如果onRejected是一个函数（必定有一个返回值x）
-        if (utils.isFunction(onRejected)) {
-          try {
-            resolve(onRejected(error));
-          } catch(ex) {
-            reject(ex);
-          }
-        } else { // 如果onRejected不是一个函数，但上一级Promise拒绝执行
-          reject(error);
-        }
-      });
-    });
-  };
-
-  /**
-   * iPromise.all 别名：iPromise.when
-   * 当promise列表中的所有promise都满足的时候才满足
-   * @param {Array} promises
-   * @return {iPromise}
-   */
-  iPromise.all = iPromise.when = function(promises) {
-    return new iPromise(function(resolve, reject) {
-    });
-  };
-
-  /**
-   * iPromise.race 别名：iPromise.once
-   * 当promise列表中有一个promise满足时就满足
-   * @param {Array} promises
-   * @return {iPromise}
-   */
-  iPromise.race = iPromise.once = function(promises) {};
-
-  /**
-   * 将传入参数转换为iPromise对象
-   * @param {Any} any
-   * @return {iPromise}
-   */
-  iPromise.resolve = function(any) {};
-
-  /**
-   * 将传入参数转换为iPromise对象，状态为rejected
-   * @param {Any} any
-   * @return {iPromise}
-   */
-  iPromise.reject = function(any) {
-    return new iPromise(function(resolve, reject) {
-      reject(any);
-    });
-  };
-
-  var handle = function(handler) {
-    var state = this.state;
-    switch(state) {
-      case PENDING: this.handlers.push(handler);break;
-      case FULFILLED: handler.onFulfilled.call(null, this.value);break;
-      case REJECTED: handler.onRejected.call(null, this.value);break;
-      default: throw new Error('The state number is illegal.');
-    }
-  };
-
-  /**
-   * 确保onFulfilled和onRejected函数只执行其中一个一次
-   */
   function doResolve(fn, onFulfilled, onRejected) {
     var done = false;
     try {
@@ -178,5 +31,107 @@
       onRejected(ex)
     }
   }
+
+  function iPromise(fn) {
+    if (!this instanceof iPromise) return new iPromise(fn);
+
+    var state = PENDING;
+    var value = null;
+    var handlers = [];
+
+    function fulfill(result) {
+      state = FULFILLED;
+      value = result;
+      handlers.forEach(handle);
+      handlers = null;
+    }
+
+    function reject(error) {
+      state = REJECTED;
+      value = error;
+      handlers.forEach(handle);
+      handlers = null;
+    }
+
+    function resolve(result) {
+      try {
+        var then = getThen(result);
+        if (then) {
+          doResolve(then.bind(result), resolve, reject)
+          return
+        }
+        fulfill(result);
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function handle(handler) {
+      if (state === PENDING) {
+        handlers.push(handler);
+      } else {
+        if (state === FULFILLED && typeof handler.onFulfilled === 'function') {
+          handler.onFulfilled(value);
+        }
+        if (state === REJECTED && typeof handler.onRejected === 'function') {
+          handler.onRejected(value);
+        }
+      }
+    }
+
+    this.done = function (onFulfilled, onRejected) {
+      setTimeout(function () {
+        handle({
+          onFulfilled: onFulfilled,
+          onRejected: onRejected
+        });
+      }, 0);
+    }
+
+    doResolve(fn, resolve, reject);
+
+  }
+
+  iPromise.prototype.then = function(onFulfilled, onRejected) {
+    var self = this;
+    return new iPromise(function (resolve, reject) {
+      return self.done(function (result) {
+        if (typeof onFulfilled === 'function') {
+          try {
+            return resolve(onFulfilled(result));
+          } catch (ex) {
+            return reject(ex);
+          }
+        } else {
+          return resolve(result);
+        }
+      }, function (error) {
+        if (typeof onRejected === 'function') {
+          try {
+            return resolve(onRejected(error));
+          } catch (ex) {
+            return reject(ex);
+          }
+        } else {
+          return reject(error);
+        }
+      });
+    });
+  };
+
+  ///// 静态方法 /////
+  iPromise.resolve = function() {};
+
+  iPromise.reject = function() {};
+
+  iPromise.all = function() {};
+
+  iPromise.race = function() {};
+
+  ///// 静态方法别名 /////
+  iPromise.when = iPromise.all;
+  iPromise.one = iPromise.race;
+
+  return iPromise;
 
 })();
