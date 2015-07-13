b355e73b0db82f95d7778b9c74835e9e465eae1b
zcoding <wuzijie_1991@163.com>
2015-07-13 17:00:10 +0800

    koa server

--- scripts/server.js
+++ scripts/server.js
@@ -1,17 +1,24 @@
 var koa = require('koa');
 var app = koa();
+var router = require('koa-router')();
+var serve = require('koa-static');
+var path = require('path');
 
-app.use(function *(next){
+var responseTime = function *(next){
   var start = new Date;
   yield next;
   var ms = new Date - start;
   console.log('%s %s - %s', this.method, this.url, ms);
+};
+
+router.get('/', function *(next) {
+  this.body = 'Hello World!';
 });
 
-// response
-
-app.use(function *(){
-  this.body = 'Hello World';
-});
+app.use(serve(path.resolve(__dirname, '../demo')))
+  .use(serve(path.resolve(__dirname, '../src')))
+  .use(responseTime)
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(9090);
